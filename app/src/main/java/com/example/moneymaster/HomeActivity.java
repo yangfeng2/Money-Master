1c6ed72d642c84aa8bd122965f5d6f586538de18
michelle <michellejia092gmail.com>
2019-04-05 17:53:50 +1100

    *home page activity created : #Scoll view #Floating Action button created
    *Created Second Main activity using Tab view activity.

--- app/src/main/java/com/example/moneymaster/HomeActivity.java
+++ app/src/main/java/com/example/moneymaster/HomeActivity.java
@@ -1,6 +1,30 @@
 package com.example.moneymaster;
 
-public class HomeActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+
+        TextView textviewDate = findViewById(R.id.text_date);
+        textviewDate.setText(currentDate);
+
+
+
+
+
+
+    }
 
 
 }
---
+++ app/src/main/java/com/example/moneymaster/Tab1Income.java
@@ -0,0 +1,17 @@
+package com.example.moneymaster;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Income extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1income, container, false);
+
+        return rootView;
+    }
+}
